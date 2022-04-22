da98768a2ce58ca1a806bf3a8d99e0c2941c6e6e
Parthtrap <mttparth306@gmail.com>
2022-04-22 23:53:34 +0530

    Minor bug fixes

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import Navbar from "./Components/Navbar";
 import HomePage from "./Pages/HomePage";
 
 function App() {
-  return (
-    <>
-      <Switch>
-        <Route exact path="/">
-          <Navbar login={true} />
-          <HomePage />
-        </Route>
-      </Switch>
-    </>
-  );
+	return (
+		<>
+			<Switch>
+				<Route exact path="/">
+					<Navbar login={true} />
+					<HomePage />
+				</Route>
+			</Switch>
+		</>
+	);
 }
 
 export default App;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,38 +1,55 @@
+import { useState } from "react";
 import "./Navbar.css";
 
 const Navbar = (props) => {
-  return (
-    <div className="navbar-main-div">
-      <div className="navbar-body">
-        <div className="navbar-body-title">
-          <a href="./">Cyber Impact</a>
-        </div>
+	const [dropdownListStyle, setDropdownListStyle] = useState({});
+	const [isOpen, setIsOpen] = useState(false);
 
-        {props.login ? (
-          <>
-            <button
-              className="navbar-dropdown-button"
-              onClick={dropdownHandler}
-            >
-              ≡
-            </button>
-            <div className="navbar-body-components">
-              <a href="./UserLogin">
-                <div className="navbar-body-components-item">Log In</div>
-              </a>
-            </div>
-          </>
-        ) : (
-          ""
-        )}
-      </div>
-      <div style={dropdownListStyle} className="navbar-dropdown">
-        <a href="./UserLogin">
-          <div className="navbar-dropdown-items">Log In</div>
-        </a>
-      </div>
-    </div>
-  );
+	const dropdownHandler = () => {
+		if (isOpen) {
+			setDropdownListStyle({
+				height: "0px",
+				animation: "drop-up-animation 1s",
+			});
+			setIsOpen(false);
+		} else {
+			setDropdownListStyle({
+				height: "60px",
+				animation: "drop-down-animation 1s",
+			});
+			setIsOpen(true);
+		}
+	};
+
+	return (
+		<div className="navbar-main-div">
+			<div className="navbar-body">
+				<div className="navbar-body-title">
+					<a href="./">Cyber Impact</a>
+				</div>
+
+				{props.login ? (
+					<>
+						<button className="navbar-dropdown-button" onClick={dropdownHandler}>
+							≡
+						</button>
+						<div className="navbar-body-components">
+							<a href="./UserLogin">
+								<div className="navbar-body-components-item">Log In</div>
+							</a>
+						</div>
+					</>
+				) : (
+					""
+				)}
+			</div>
+			<div style={dropdownListStyle} className="navbar-dropdown">
+				<a href="./UserLogin">
+					<div className="navbar-dropdown-items">Log In</div>
+				</a>
+			</div>
+		</div>
+	);
 };
 
 export default Navbar;
