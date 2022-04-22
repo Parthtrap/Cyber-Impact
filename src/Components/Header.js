834c5ed7eaa9253a113d2b8ab99b579e09595d97
Ariyan Seikh <ariyansk2002@gmail.com>
2022-04-23 01:29:04 +0530

    Added Market Profile button to header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,48 +2,58 @@ import { useState } from "react";
 import "./Header.css";
 
 const Header = (props) => {
-	const [dropdownListStyle, setDropdownListStyle] = useState({});
-	const [isOpen, setIsOpen] = useState(false);
+  const [dropdownListStyle, setDropdownListStyle] = useState({});
+  const [isOpen, setIsOpen] = useState(false);
 
-	const dropdownHandler = () => {
-		if (isOpen) {
-			setDropdownListStyle({
-				height: "0px",
-				animation: "drop-up-animation 1s",
-			});
-			setIsOpen(false);
-		} else {
-			setDropdownListStyle({
-				height: "119px",
-				animation: "drop-down-animation 1s",
-			});
-			setIsOpen(true);
-		}
-	};
+  const dropdownHandler = () => {
+    if (isOpen) {
+      setDropdownListStyle({
+        height: "0px",
+        animation: "drop-up-animation 1s",
+      });
+      setIsOpen(false);
+    } else {
+      setDropdownListStyle({
+        height: "119px",
+        animation: "drop-down-animation 1s",
+      });
+      setIsOpen(true);
+    }
+  };
 
-	return (
-		<div className="header-main-div">
-			<div className="header-body">
-				<div className="header-body-title">
-					<a href="./">Cyber Impact</a>
-				</div>
+  return (
+    <div className="header-main-div">
+      <div className="header-body">
+        <div className="header-body-title">
+          <a href="./">Cyber Impact</a>
+        </div>
 
-				<button className="header-dropdown-button" onClick={dropdownHandler}>
-					≡
-				</button>
-				<div className="header-body-components">
-					<a href="./UserLogin">
-						<div className="header-body-components-item">Log Out</div>
-					</a>
-				</div>
-			</div>
-			<div style={dropdownListStyle} className="header-dropdown">
-				<a href="./UserLogin">
-					<div className="header-dropdown-items">Log Out</div>
-				</a>
-			</div>
-		</div>
-	);
+        <button className="header-dropdown-button" onClick={dropdownHandler}>
+          ≡
+        </button>
+
+        {/* Buttons for Header */}
+        <div className="header-buttons">
+          <div className="header-body-components">
+            <a href="./UserLogin">
+              <div className="header-body-components-item">Market Profile</div>
+            </a>
+          </div>
+
+          <div className="header-body-components">
+            <a href="./UserLogin">
+              <div className="header-body-components-item">Log Out</div>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div style={dropdownListStyle} className="header-dropdown">
+        <a href="./UserLogin">
+          <div className="header-dropdown-items">Log Out</div>
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
