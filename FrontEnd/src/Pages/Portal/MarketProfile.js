cdcc8da0e1cf1f7c80f21b9dd3ad30a519763c80
Kitashi <rishavrajaryan14@gmail.com>
2022-04-23 18:30:55 +0530

    market profiel route setting

--- FrontEnd/src/Pages/Portal/MarketProfile.js
+++ FrontEnd/src/Pages/Portal/MarketProfile.js
@@ -1,21 +1,24 @@
 import "./MarketProfile.css";
 
 const MarketProfile = () => {
-	return (
-		<div className="market-profile-body">
-			<form className="market-profile-bar" action="./ProfileForm">
-				<button id="market-profile-new-profile-btn" className="market-profile-new-profile-btn">
-					Add New Profile
-					<span></span>
-					<span></span>
-					<span></span>
-					<span></span>
-				</button>
-			</form>
+  return (
+    <div className="market-profile-body">
+      <form className="market-profile-bar" action="./ProfileForm">
+        <button
+          id="market-profile-new-profile-btn"
+          className="market-profile-new-profile-btn"
+        >
+          Add New Profile
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+        </button>
+      </form>
 
-			<div className="market-profile-main-display"></div>
-		</div>
-	);
+      <div className="market-profile-main-display"></div>
+    </div>
+  );
 };
 
 export default MarketProfile;
