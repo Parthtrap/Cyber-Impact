74a32e22d9536e6b68ea16c904320aab479335f2
Kitashi <rishavrajaryan14@gmail.com>
2022-04-23 14:52:11 +0530

    api for userdata added

--- BankEnd/server/server.js
+++ BankEnd/server/server.js
@@ -3,6 +3,9 @@ const express = require("express");
 const app = express();
 const port = 6000;
 
+//extracting routers
+const userRouters = require("./routers/user-routers");
+
 // for parsering json file
 const bodyParser = require("body-parser");
 app.use(bodyParser.json());
@@ -14,6 +17,19 @@ app.use(express.static("public"));
 const mongoose = require("mongoose");
 const dbName = "cyberImpactDB";
 mongoose.connect(`mongodb://localhost:27017/${dbName}`);
+
+//adding a middleware for setting headers in api requests for allowing its execution to from another server when using browsers
+app.use((req, res, next) => {
+	res.setHeader("Access-Control-Allow-Origin", "*");
+	res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+	res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+
+	next();
+});
+
+//setting api
+app.use("/api/user", userRouters);
+// app.use("/api/market", marketRouters);
 
 //setting the server port
 app.listen(port, () => console.log(`Server listening on port ${port}`));
