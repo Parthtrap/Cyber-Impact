6ab7cee14beb9cde28c1752d11888e640ae9d1d5
Kitashi <rishavrajaryan14@gmail.com>
2022-04-23 18:19:11 +0530

    add-market api added

--- BankEnd/server/controllers/markets-controller.js
+++ BankEnd/server/controllers/markets-controller.js
@@ -2,4 +2,58 @@
 const Market = require("../models/market");
 
 //post newMarketProfile request function
-const addMarket = (req, res) => {};
+const addMarket = async (req, res) => {
+
+    //destructuring and storing requested data
+    const {title, phonenum, address, lat, lng, openingTime, closingTime, profession, ownerId} = req.body;
+
+    //finding existing market with same name and owner;
+    let existingMarket;
+    try {
+        existingMarket = await Market.findOne({
+            title : title,
+            ownerId : ownerId
+        });
+    } catch(err){
+        console.log(err.message);
+        res.status(500).json({ error: err.message});
+    }
+
+    //checking existing market
+    if(existingMarket) {
+        console.log("This user already owns a market with this name. Choose a different name");
+        res.status(422).json({ error : "This user already owns a market with this name. Choose a different name"});
+    }
+
+    //creating new market 
+    const newMarket = new Market({
+        title : title,
+        phonenum : phonenum,
+        address : address,
+        location : {
+            lat : lat,
+            lng : lng
+        },
+        description : null,
+        rating : 0,
+        reviews : [],
+        openingTime : openingTime,
+        closingTime : closingTime,
+        profession : profession,
+        ownerId : ownerId
+    });
+
+    //add newMarket to database
+    try {
+        await newMarket.save();
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({error : err.message});
+    }
+
+    //resending data with 'OK' status code
+    res.status(201).json({market : newMarket});
+
+};
+
+exports.addMarket = addMarket;
