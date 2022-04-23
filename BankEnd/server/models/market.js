3d1132a5c77ff36cb0f5cc1c9116ccb30c3fe7d0
Kitashi <rishavrajaryan14@gmail.com>
2022-04-23 14:46:48 +0530

    models for database collections added

---
+++ BankEnd/server/models/market.js
@@ -0,0 +1,45 @@
+//extracting mongoose module
+const mongoose = require("mongoose");
+
+//Schema for reviews
+const reviewSchema = new mongoose.Schema ({
+
+    raterID : {
+        type : mongoose.Types.ObjectId, 
+        required : true,
+        ref : "User"
+    },
+    rating : {
+        type : Number,
+        required : true,
+    },
+    review : {
+        type : String,
+        maxlength : 500
+    }
+})
+
+
+//Schema for markets
+const marketSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  phonenum : {
+      type : Number,
+      required : true
+  },
+  imageURL: { type: String, required: true },
+  address: { type: String, required: true },
+  location: {
+    lat: { type: Number, required: true },
+    lng: { type: Number, required: true },
+  },
+  description: { type: String, required: true },
+  rating : {
+      type : Number
+  },
+  reviews : [reviewSchema],
+  owner: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+});
+
+//exporting Market modal
+module.exports = mongoose.module("Market", marketSchema);
---
+++ BankEnd/server/models/user.js
@@ -0,0 +1,34 @@
+//extracting mongoose module
+const mongoose = require("mongoose");
+
+//Schema for users
+const userSchema = new mongoose.Schema({
+    username : {
+        type : String,
+        required : [true , "Please enter username"],
+        unique : true,
+        maxlength : 25
+    },
+    email : {
+        type : String,
+        required : [true, "Please enter the email"],
+        unique :true 
+    },
+    password : {
+        type : String,
+        required : [true, "Please enter a password"],
+        minlength : 6
+    },
+    favourites : [{
+        type : mongoose.Types.ObjectId, 
+        ref : "Market"
+    }],
+    markets : [{
+        type : mongoose.Types.ObjectId,
+        ref: "Market"
+    }]
+
+});
+
+//exporting User modal
+module.exports = mongoose.model('User', userSchema);
