59e22cf638704944b27d628d2e6fd8cda55a1175
Parthtrap <mttparth306@gmail.com>
2022-04-23 18:24:08 +0530

    Commenting

--- FrontEnd/src/Pages/Authentication/LoginPage.js
+++ FrontEnd/src/Pages/Authentication/LoginPage.js
@@ -5,25 +5,32 @@ import { ValidateEmail } from "../../HelperClasses/HelperFunctions";
 import "./LoginPage.css";
 
 const LoginPage = () => {
+	// Calling the Context
 	const auth = useContext(AuthContext);
 
+	// Initiating Refrences
 	const emailInputRef = useRef();
 	const passwordInputRef = useRef();
 
-	const submitPressed = async (event) => {
-		event.preventDefault();
+	// Submit Button On Click Handler
+	const submitButtonHandler = async (event) => {
+		event.preventDefault(); // Preventing Page Reloading
 
+		// Getting values from edit texts
 		const Email = emailInputRef.current.value;
 		const Password = passwordInputRef.current.value;
 
+		// Check for Valid Email and Password Length
 		if (!ValidateEmail(Email)) alert("Enter a Valid Email!");
 		else if (Password.length < 8) alert("Password should be atlest 8 characters long!");
 		else {
+			// Email and Password are Valid
 			try {
 				const userdata = JSON.stringify({
 					email: Email,
 					password: Password,
 				});
+
 				const response = await fetch("http://localhost:5000/api/user/login", {
 					method: "POST",
 					headers: {
@@ -31,12 +38,16 @@ const LoginPage = () => {
 					},
 					body: userdata,
 				});
+
 				const responseData = await response.json();
+
+				// Email Password Matches => Login
 				if (response.status === 201) {
 					auth.login({
 						username: responseData.user.username,
 						id: responseData.user._id,
 					});
+
 					console.log(responseData);
 				} else {
 					console.log(responseData.error);
@@ -69,7 +80,7 @@ const LoginPage = () => {
 						<div className="pass">
 							<Link to="./forgotpassword">Forgot Password?</Link>
 						</div>
-						<input className="login-submit-button" onClick={submitPressed} type="submit" value="Login" />
+						<input className="login-submit-button" onClick={submitButtonHandler} type="submit" value="Login" />
 						<div className="login-page-signup-link">
 							Not Signed Up? <Link to="./Signup">User Signup</Link>
 						</div>
